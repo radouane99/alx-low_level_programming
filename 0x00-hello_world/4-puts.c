dd8f2e18d250a80c04f9e3783ba3ee552b235aa3
Radouane <radouane.asri99@gmail.com>
2023-08-10 10:02:21 -0700

    Fix coding style in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,8 @@
  * new line ."
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
