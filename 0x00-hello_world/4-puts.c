1d7e2af7c7478c5338abc89d3f302ff02f1f3e12
Radouane <radouane.asri99@gmail.com>
2023-08-10 10:01:05 -0700

    Fix coding style in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
+ * main prints " programming like building a multilingual
+ * new line ."
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
