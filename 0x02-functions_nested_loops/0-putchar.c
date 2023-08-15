673f22d8bed4a1a4cdb03c5539193903019bd272
Radouane <radouane.asri99@gmail.com>
2023-08-15 14:42:27 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,15 @@
  */
 int main(void)
 {
-  char *sh="holberton";
-  while(*sh){
-    _putchar(*sh);
-    sh++;
-  }
-  _putchar('\n');
-  return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
