a0351846576097c7c471b222251e6bd11ea7390c
Radouane <radouane.asri99@gmail.com>
2023-08-15 15:57:30 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_alphabet - prints alphabet
  *
@@ -8,10 +8,9 @@
 void print_alphabet(void)
 {
 char i;
-
 for (i = 'a'; i <= 'z'; i++)
 {
-	_putchar(i);
+_putchar(i);
 }
 _putchar('\n');
 }
