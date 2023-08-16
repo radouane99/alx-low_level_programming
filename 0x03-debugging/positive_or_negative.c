083bb13b26276d8c40945f4b970937cad1ff8e8b
Radouane <radouane.asri99@gmail.com>
2023-08-16 08:05:13 -0700

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * Positive_or_negative - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
 void positive_or_negative(int i){
 if(i>0){
 printf("%d is positive \n",i);
