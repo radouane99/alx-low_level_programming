9db24dca2020c8989a1d3b425c145c6a1ce5c869
Radouane <radouane.asri99@gmail.com>
2023-08-17 02:03:25 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,7 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 
 /*while (i < 10)*/
@@ -18,6 +16,5 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
