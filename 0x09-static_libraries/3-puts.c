0cf83e91dd3fe96e51b54c198d25869b975da0c2
Radouane <radouane.asri99@gmail.com>
2023-09-01 03:34:47 -0700

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _strncpy - Copies at most an inputted number of bytes
 * from string src into dest.
@@ -13,7 +12,6 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 
 i = 0;
-
 while (src[i] != '\0' && i < n)
 {
 dest[i] = src[i];
